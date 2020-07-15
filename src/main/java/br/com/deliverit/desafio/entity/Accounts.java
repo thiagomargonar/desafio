84ca24be3280d99f9944177203d4d927db86b963
Thiago Margonar <thiago.margonar@hotmail.com>
2020-07-15 18:11:00 -0300

    Desafio #in progress #time .1h 00m #comment InitCommit

--- src/main/java/br/com/deliverit/desafio/entity/Accounts.java
+++ src/main/java/br/com/deliverit/desafio/entity/Accounts.java
@@ -3,10 +3,7 @@ package br.com.deliverit.desafio.entity;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.util.Date;
 
 @Entity
 @Data
@@ -16,20 +13,5 @@ public class Accounts implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @NotNull
-    private String name;
-
-    @NotNull
-    private BigDecimal value;
-
-    @NotNull
-    private BigDecimal valueAdjusted;
-
-    @NotNull
-    private Date expiration;
-
-    @NotNull
-    private Long expirationDays;
 
 }
