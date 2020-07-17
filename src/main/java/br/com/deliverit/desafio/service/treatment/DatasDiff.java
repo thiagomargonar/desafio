aa062e528ff334dbcac2bdeee27c427adb6f5539
Thiago Margonar <thiago.margonar@hotmail.com>
2020-07-17 18:56:48 -0300

    Task-01 #available #time 0h 15m 0s #comment ajustado regras de datas

--- src/main/java/br/com/deliverit/desafio/service/treatment/DatasDiff.java
+++ src/main/java/br/com/deliverit/desafio/service/treatment/DatasDiff.java
@@ -1,57 +1,21 @@
 package br.com.deliverit.desafio.service.treatment;
 
-import java.util.GregorianCalendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DatasDiff {
 
     public Long dataDiff(java.util.Date dataLow, java.util.Date dataHigh){
 
-        GregorianCalendar startTime = new GregorianCalendar();
-        GregorianCalendar endTime = new GregorianCalendar();
-
-        GregorianCalendar curTime = new GregorianCalendar();
-        GregorianCalendar baseTime = new GregorianCalendar();
-
-        startTime.setTime(dataLow);
-        endTime.setTime(dataHigh);
-
-        int dif_multiplier = 1;
-
-        // Verifica a ordem de inicio das datas
-        if( dataLow.compareTo( dataHigh ) > 0 ){
-            baseTime.setTime(dataHigh);
-            curTime.setTime(dataLow);
-            dif_multiplier = 1;
-        }else{
-            baseTime.setTime(dataLow);
-            curTime.setTime(dataHigh);
-            dif_multiplier = -1;
-        }
-
-        int result_years = 0;
-        int result_months = 0;
-        int result_days = 0;
-
-        // Para cada mes e ano, vai de mes em mes pegar o ultimo dia para import acumulando
-        // no total de dias. Ja leva em consideracao ano bissesto
-        while( curTime.get(GregorianCalendar.YEAR) < baseTime.get(GregorianCalendar.YEAR) ||
-                curTime.get(GregorianCalendar.MONTH) < baseTime.get(GregorianCalendar.MONTH)  )
-        {
-
-            int max_day = curTime.getActualMaximum( GregorianCalendar.DAY_OF_MONTH );
-            result_months += max_day;
-            curTime.add(GregorianCalendar.MONTH, 1);
-
-        }
-
-        // Marca que é um saldo negativo ou positivo
-        result_months = result_months*dif_multiplier;
-
-
-        // Retirna a diferenca de dias do total dos meses
-        result_days += (endTime.get(GregorianCalendar.DAY_OF_MONTH) - startTime.get(GregorianCalendar.DAY_OF_MONTH));
-
-        return Long.valueOf((result_years+result_months+result_days)*-1);
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        df.setLenient(false);
+        Date d1 = dataLow;
+        System.out.println (d1);
+        Date d2 = dataHigh;
+        System.out.println (d2);
+        long dt = (d2.getTime() - d1.getTime()) + 3600000;
+        return ((dt / 86400000L)*-1);
     }
 
 }
